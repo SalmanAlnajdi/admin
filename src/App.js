30a5d196d3cf352b716744988292dd393d8d4a1d
Tourki-hub <tourkimufarej@gmail.com>
2024-07-18 10:25:27 +0300

    library setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="login" Component={Login} />
+    </Routes>
   );
 }
 
